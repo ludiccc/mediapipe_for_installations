d2fab676a227de1e4dee83b13a1abd70f5005ba9
Julia Saenz <julisaenz99@gmail.com>
2023-08-30 14:12:20 -0300

    [fix] hand_landmarks working

--- ejemplo-hand-landmarks.py
+++ ejemplo-hand-landmarks.py
@@ -16,7 +16,7 @@ args = parser.parse_args()
 client = udp_client.SimpleUDPClient(args.ip, args.port)
 
 # Initialize mediapipe
-mp_face = mp.solutions.face
+mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 
 # Initialize video capture
@@ -24,7 +24,7 @@ cap = cv2.VideoCapture(args.cam)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, args.cam_width)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, args.cam_height)
 
-with mp_face.FaceLandmark(min_detection_confidence=0.5) as face_landmark:
+with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
@@ -34,16 +34,16 @@ with mp_face.FaceLandmark(min_detection_confidence=0.5) as face_landmark:
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         # Process the image with mediapipe
-        results = face_landmark.process(rgb_frame)
+        results = hands.process(rgb_frame)
 
-        # Draw the face landmarks
-        if results.detections:
-            for detection in results.detections:
-                mp_drawing.draw_landmarks(frame, detection.landmarks, mp_face.FACE_CONNECTIONS)
+        # Draw the hand landmarks
+        if results.multi_hand_landmarks:
+            for landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(frame, landmarks, mp_hands.HAND_CONNECTIONS)
 
-                msg = osc_message_builder.OscMessageBuilder(address='face_landmarks')
+                msg = osc_message_builder.OscMessageBuilder(address='hand_landmarks')
 
-                for landmark in detection.landmarks.landmark:
+                for landmark in landmarks.landmark:
                     msg.add_arg(landmark.x)
                     msg.add_arg(landmark.y)
                     msg.add_arg(landmark.z)
@@ -52,7 +52,7 @@ with mp_face.FaceLandmark(min_detection_confidence=0.5) as face_landmark:
                 client.send(msg)
 
         # Display the frame
-        cv2.imshow('Face Landmarks', frame)
+        cv2.imshow('Hand Landmarks', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
